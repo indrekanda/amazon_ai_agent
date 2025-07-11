264b85dd262ea66179099451857397694b860a6b
indre <indrekanda@gmail.com>
2025-07-11 22:40:41 +0300

    Enhance RAG retrieval and UI with richer context and suggestions

--- src/api/api/endpoints.py
+++ src/api/api/endpoints.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 import logging
 
-from api.api.models import RAGRequest, RAGResponse
+from api.api.models import RAGRequest, RAGResponse, RAGUsedImage
 from api.rag.retrieval import rag_pipeline_wrapper
 
 
@@ -20,9 +20,12 @@ async def rag(
     
     result = rag_pipeline_wrapper(payload.query)
     
+    used_image_urls = [RAGUsedImage(image_url=image["image_url"], price=image["price"], description=image["description"]) for image in result["retrieved_images"]]
+    
     return RAGResponse(
         request_id=request.state.request_id,
-        answer=result
+        answer=result["answer"],
+        used_image_urls=used_image_urls,
     )
 
 
--- src/api/api/models.py
+++ src/api/api/models.py
@@ -6,6 +6,15 @@ from typing import List, Any, Optional
 class RAGRequest(BaseModel):
     query: str = Field(..., description="The query to be used in the RAG pipeline")
 
+
+class RAGUsedImage(BaseModel):
+    image_url: str = Field(..., description="The url of the retrieved image")
+    price: Optional[float] = Field(None, description="The price of the item")
+    description: str = Field(..., description="The description of the item")
+
+
 class RAGResponse(BaseModel):
     request_id: str = Field(..., description="The id of the request")
     answer: str = Field(..., description="The content of the RAR response")
+    used_image_urls: List[RAGUsedImage] = Field(..., description="The list of the retrieved images")
+
